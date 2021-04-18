86e182e7930998058b97a6751965ea662d1c8fd6
Artem <artem.yavd@gmail.com>
2021-04-18 10:52:34 +0300

    add src directory

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import React, { Component } from 'react'
+
+
+
+
+export class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    )
+  }
+}
+
+export default App
